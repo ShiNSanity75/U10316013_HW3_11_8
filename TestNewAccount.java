dd5e9fe1256d61874f82a13a2a15a060b7394ffa
ShiNSanity75 <jj06280705@gmail.com>
2015-03-30 15:14:20 +0800

    test 第一版

---
+++ TestNewAccount.java
@@ -0,0 +1,21 @@
+import java.util.Date;
+import java.util.ArrayList;
+
+public class TestNewAccount {
+	//main method
+	public static void main(String[] args) {
+		NewAccount myNewAccount = new NewAccount("George", 1122, 1000.0, 0.015);
+		myNewAccount.deposit(30.0);
+		myNewAccount.deposit(40.0);
+		myNewAccount.deposit(50.0);
+		myNewAccount.withdraw(5.0);
+		myNewAccount.withdraw(4.0);
+		myNewAccount.withdraw(2.0);
+		
+		System.out.println("Name: " + myNewAccount.getName());
+		System.out.println("ID: " + myNewAccount.getId());
+		System.out.println("Monthly Interest: " + myNewAccount.getMonthlyInterestRate());
+		System.out.println("Balance: " + myNewAccount.getBalance());
+		myNewAccount.getTransactions();
+	}
+}
